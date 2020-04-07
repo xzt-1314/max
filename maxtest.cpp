f817e630ead0e7222e33ce6d4837fa62ad89a55c
xzt-1314 <62635552+xzt-1314@users.noreply.github.com>
2020-04-08 01:22:43 +0800

    Create maxtest.cpp

---
+++ maxtest.cpp
@@ -0,0 +1,118 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int n = 7;
+			int a[7] = {-1,-2,-3,-4,-5,-6,-7};
+			int max = 0;
+			int add = 0;
+			for (int i = 0; i < n; i++)
+			{
+				add += a[i];
+				if (add > max)
+				{
+					max = add;
+				}
+				else if (add < 0)
+				{
+					add = 0;
+				}
+			}
+			int right = 0;
+			Assert::AreEqual(right, max);
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			int n = 7;
+			int a[7] = { 1,1,1,1,1,1,1 };
+			int max = 0;
+			int add = 0;
+			for (int i = 0; i < n; i++)
+			{
+				add += a[i];
+				if (add > max)
+				{
+					max = add;
+				}
+				else if (add < 0)
+				{
+					add = 0;
+				}
+			}
+			int right = 7;
+			Assert::AreEqual(right, max);
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			int n = 7;
+			int a[7] = {1,0,-1,-2,-3,-4,-5};
+			int max = 0;
+			int add = 0;
+			for (int i = 0; i < n; i++)
+			{
+				add += a[i];
+				if (add > max)
+				{
+					max = add;
+				}
+				else if (add < 0)
+				{
+					add = 0;
+				}
+			}
+			int right = 1;
+			Assert::AreEqual(right, max);
+		}
+		TEST_METHOD(TestMethod4)
+		{
+			int n = 7;
+			int a[7] = {-5,-4,-3,-2,-1,0,1};
+			int max = 0;
+			int add = 0;
+			for (int i = 0; i < n; i++)
+			{
+				add += a[i];
+				if (add > max)
+				{
+					max = add;
+				}
+				else if (add < 0)
+				{
+					add = 0;
+				}
+			}
+			int right = 1;
+			Assert::AreEqual(right, max);
+		}
+		TEST_METHOD(TestMethod5)
+		{
+			int n = 7;
+			int a[7] = {-2,11,-4,13,-5,-2};
+			int max = 0;
+			int add = 0;
+			for (int i = 0; i < n; i++)
+			{
+				add += a[i];
+				if (add > max)
+				{
+					max = add;
+				}
+				else if (add < 0)
+				{
+					add = 0;
+				}
+			}
+			int right = 20;
+			Assert::AreEqual(right, max);
+		}
+	};
+}
